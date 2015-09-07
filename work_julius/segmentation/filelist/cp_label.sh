faa8caad63e63628ab147a4a2fabe38c17959f6a
ideally <fujiwara@asr.info.gifu-u.ac.jp>
2015-09-07 16:52:26 +0900

    change files in /work_julius/segmentation/filelist

---
+++ work_julius/segmentation/filelist/cp_label.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#cp /home/ideally/User/DBNF-GMM-HMM/work_julius/segmentation/transcription/test/*.lab ../work
+cp /home/ideally/User/DBNF-GMM-HMM/work_julius/segmentation/transcription/train/*.lab ../work
